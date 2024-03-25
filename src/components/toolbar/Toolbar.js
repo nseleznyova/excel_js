ea5a5cbb423e7ee998c0ac3f77c11fbebbf5792f
Наталья Селезнева <natand8177@gmail.com>
2024-03-25 18:52:04 +0200

    [fix] fixed bug with styles

--- src/components/toolbar/Toolbar.js
+++ src/components/toolbar/Toolbar.js
@@ -10,7 +10,7 @@ export class Toolbar extends ExcelStateComponent {
     super($root, {
       name: 'Toolbar',
       listeners: ['click'],
-      subscribe: ['currentStyles'],
+      subscribeState: ['currentStyles'],
       ...options,
     })
   }
